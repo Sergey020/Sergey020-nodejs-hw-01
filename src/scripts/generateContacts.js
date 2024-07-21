62858061c03b6b5e71e0c4225a3fefd883964f44
Sergey020 <114489697+Sergey020@users.noreply.github.com>
2024-07-21 16:17:43 +0300

    create generate contacts

--- src/scripts/generateContacts.js
+++ src/scripts/generateContacts.js
@@ -1,5 +1,19 @@
 import { PATH_DB } from '../constants/contacts.js';
+import { createFakeContact } from '../utils/createFakeContacts.js';
+import fs from 'node:fs/promises';
 
-const generateContacts = async (number) => {};
+const generateContacts = async (number) => {
+try{
+    const data = await fs.readFile(PATH_DB, 'utf-8');
+    const parsedData = JSON.parse(data);
+    for (let i = 1; i <= number; i++) {
+        const contact = createFakeContact();
+        parsedData.push(contact);
+    }
+    fs.writeFile(PATH_DB, JSON.stringify(parsedData, null, 2));
+  } catch (error) {
+    console.error('Error generating contacts:', error);
+}
+};
 
 generateContacts(5);
