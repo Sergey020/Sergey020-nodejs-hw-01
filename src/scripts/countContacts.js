881115a36dbcec34c7f89b9dd10e0ec6b6088fb2
Sergey020 <114489697+Sergey020@users.noreply.github.com>
2024-07-21 20:41:34 +0300

    update

--- src/scripts/countContacts.js
+++ src/scripts/countContacts.js
@@ -2,9 +2,13 @@ import { PATH_DB } from '../constants/contacts.js';
 import fs from 'node:fs/promises';
 export const countContacts = async () => {
     try{
+        let count = 0;
         const data = await fs.readFile(PATH_DB, 'utf-8');
         const parsedData = JSON.parse(data);
-            return parsedData;
+        for (let i = 0; i < parsedData.length; i++) {
+            count += 1;
+          }
+            return count;
       } catch (error) {
         console.error('Error generating contacts:', error);
     }
